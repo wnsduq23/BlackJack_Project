acc18eb42a3fe7d05d4db9463c1e18ed2bcff0c0
songhyungjin <sori4606@naver.com>
2023-05-29 12:55:00 +0900

    test

--- blackjack_windform/StartPage.Designer.cs
+++ blackjack_windform/StartPage.Designer.cs
@@ -28,11 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             label1 = new Label();
             pictureBox1 = new PictureBox();
             pictureBox2 = new PictureBox();
             button1 = new Button();
             button2 = new Button();
+            imageList1 = new ImageList(components);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
@@ -41,9 +43,10 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("돋움", 20F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(222, 25);
+            label1.Location = new Point(155, 15);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(370, 40);
+            label1.Size = new Size(257, 27);
             label1.TabIndex = 0;
             label1.Text = "B L A C K J A C K";
             label1.TextAlign = ContentAlignment.TopCenter;
@@ -52,9 +55,10 @@
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.카드이미지;
-            pictureBox1.Location = new Point(300, 91);
+            pictureBox1.Location = new Point(213, 55);
+            pictureBox1.Margin = new Padding(2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(197, 120);
+            pictureBox1.Size = new Size(138, 72);
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             pictureBox1.Click += pictureBox1_Click;
@@ -62,9 +66,10 @@
             // pictureBox2
             // 
             pictureBox2.Image = Properties.Resources.a스페이스카드;
-            pictureBox2.Location = new Point(75, 241);
+            pictureBox2.Location = new Point(51, 145);
+            pictureBox2.Margin = new Padding(2);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(114, 149);
+            pictureBox2.Size = new Size(80, 89);
             pictureBox2.TabIndex = 2;
             pictureBox2.TabStop = false;
             pictureBox2.Click += pictureBox2_Click;
@@ -72,9 +77,10 @@
             // button1
             // 
             button1.Font = new Font("맑은 고딕", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            button1.Location = new Point(274, 242);
+            button1.Location = new Point(192, 145);
+            button1.Margin = new Padding(2);
             button1.Name = "button1";
-            button1.Size = new Size(255, 63);
+            button1.Size = new Size(178, 38);
             button1.TabIndex = 3;
             button1.Text = "PLAY";
             button1.UseVisualStyleBackColor = true;
@@ -83,26 +89,34 @@
             // button2
             // 
             button2.Font = new Font("맑은 고딕", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            button2.Location = new Point(274, 327);
+            button2.Location = new Point(192, 196);
+            button2.Margin = new Padding(2);
             button2.Name = "button2";
-            button2.Size = new Size(255, 63);
+            button2.Size = new Size(178, 38);
             button2.TabIndex = 4;
             button2.Text = "EXIT";
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
-            // MainForm
+            // imageList1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            imageList1.ColorDepth = ColorDepth.Depth8Bit;
+            imageList1.ImageSize = new Size(16, 16);
+            imageList1.TransparentColor = Color.Transparent;
+            // 
+            // StartPage
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DarkGreen;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(560, 270);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(pictureBox2);
             Controls.Add(pictureBox1);
             Controls.Add(label1);
-            Name = "MainForm";
+            Margin = new Padding(2);
+            Name = "StartPage";
             Text = "Start Page";
             Load += MainForm_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
@@ -118,5 +132,6 @@
         private PictureBox pictureBox2;
         private Button button1;
         private Button button2;
+        private ImageList imageList1;
     }
 }
--- blackjack_windform/StartPage.cs
+++ blackjack_windform/StartPage.cs
@@ -1,3 +1,17 @@
+using Blackjack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using blackjack_windform.Properties;
+
+using BP = Blackjack.Program;
+
 namespace blackjack_windform
 {
     public partial class StartPage : Form
@@ -17,8 +31,80 @@ namespace blackjack_windform
 
         }
 
+        public static Image[] cardImage;
         private void MainForm_Load(object sender, EventArgs e)
         {
+            for (int i = 1; i <= 13; i++)
+            {
+                BP.PushInformationCard('c', i);
+            }
+            for (int i = 1; i <= 13; i++)
+            {
+                BP.PushInformationCard('d', i);
+            }
+            for (int i = 1; i <= 13; i++)
+            {
+                BP.PushInformationCard('h', i);
+            }
+            for (int i = 1; i <= 13; i++)
+            {
+                BP.PushInformationCard('s', i);
+            }
+
+            /*            for (int i = 0; i <= 51; i++)
+                        {
+                            MessageBox.Show(Blackjack.Program.all_card[i].number.ToString());
+                        }*/
+
+            Image bmp = new Bitmap(blackjack_windform.Properties.Resources.카드전체); //이미지 데이터 저장할 때 쓰는 클래스
+
+            Image spriteSheet = bmp;
+
+            int cardWidth = 167; //카드 1개 폭
+            int cardHeight = 244; //카드 1개 높이
+
+            int spriteSheetWidth = spriteSheet.Width; // 전체 폭
+            int spriteSheetHeight = spriteSheet.Height; // 전체 높이
+
+
+            int cardsPerRow = spriteSheetWidth / cardWidth; //13
+            int cardsPerColumn = spriteSheetHeight / cardHeight; //5
+
+            
+            cardImage = new Image[cardsPerColumn * cardsPerRow];
+
+
+            // 각 카드의 이미지를 배열에 저장
+            for (int y = 0; y < cardsPerColumn; y++)
+            {
+                for (int x = 0; x < cardsPerRow; x++)
+                {
+                    Rectangle cropRect = new Rectangle(x * cardWidth, y * cardHeight, cardWidth, cardHeight);
+                    Bitmap croppedImage = new Bitmap(cardWidth, cardHeight);
+                    Graphics graphics = Graphics.FromImage(croppedImage);
+                    graphics.DrawImage(spriteSheet, new Rectangle(0, 0, cardWidth, cardHeight), cropRect, GraphicsUnit.Pixel);
+                    int index = y * cardsPerRow + x;
+                    cardImage[index] = croppedImage;
+                }
+            }
+
+            BP.Shuffle();
+
+
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox.Location = new Point(10, 10); // PictureBox의 위치 설정
+            pictureBox.Size = new Size(cardWidth, cardHeight); // PictureBox의 크기 설정
+
+            // 카드 이미지를 PictureBox에 표시
+            pictureBox.Image = cardImage[13];
+            MessageBox.Show(BP.all_card[13].number.ToString());
+            MessageBox.Show(BP.all_card[13].shape.ToString());
+
+            // Form에 PictureBox 추가
+            this.Controls.Add(pictureBox);
+
+            
 
         }
 
@@ -35,7 +121,7 @@ namespace blackjack_windform
         private void button1_Click(object sender, EventArgs e)
         {
             this.Visible = false;
-            GamePage showForm2=new GamePage();
+            GamePage showForm2 = new GamePage();
             showForm2.ShowDialog();
         }
 
