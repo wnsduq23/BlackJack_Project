bc61c3d0d8b70e94e9d9ecd5b480957a60d790d9
Kim Jun Yeop <wnsduq23@naver.com>
2023-06-01 17:02:40 +0900

    add PairBetting.cs

---
+++ blackjack_windform/PairBetting.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace blackjack_windform
+{
+    partial class PairBetting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(102, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(149, 45);
+            label1.TabIndex = 0;
+            label1.Text = "얼마를 베팅하시겠습니까?\r\n(현재 소유 금액 : 200)\r\n숫자로 작성해주세요.";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(102, 81);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(135, 23);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(257, 81);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "베팅하기";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // PairBetting
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(354, 149);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "PairBetting";
+            Text = "PairBetting";
+            Load += PairBetting_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
---
+++ blackjack_windform/PairBetting.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BP = Blackjack.Program;
+
+namespace blackjack_windform
+{
+    public partial class PairBetting : Form
+    {
+        public PairBetting()
+        {
+            InitializeComponent();
+        }
+
+        private void PairBetting_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //pair_bet textbox에 값 집어넣기
+            //textbox2.Text = BP.pair_bet.ToString();
+            BP.pair_bet = int.Parse(textBox1.Text);
+            this.Close();
+        }
+    }
+}
